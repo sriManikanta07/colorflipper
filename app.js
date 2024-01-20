4a64054a5e3b567136f9081d980c079a32c32280
sriManikanta07 <144330872+sriManikanta07@users.noreply.github.com>
2024-01-20 20:25:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener("click",function(){
+   const randomnumber = getRandomNumber();
+       console.log(randomnumber);
+   document.body.style.backgroundColor = colors[randomnumber]
+   color.textContent = colors[randomnumber];
+
+
+
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length);
+} 
